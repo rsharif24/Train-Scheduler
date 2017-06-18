0586ba62a219178313a8511c653928b28b6647d1
Ruba <rsharif24@hotmail.com>
2017-06-17 22:16:13 -0500

    added html and firebase js code

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+
+    var config = {
+        apiKey: "AIzaSyBn0GKgliQXh158x4AwR9okB-lbTeqP5lY",
+        authDomain: "train-schedular-dfb49.firebaseapp.com",
+        databaseURL: "https://train-schedular-dfb49.firebaseio.com",
+        projectId: "train-schedular-dfb49",
+        storageBucket: "",
+        messagingSenderId: "463179035205"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+    // var name = "";
+    // var destination = "";
+    // var firstTrain = "";
+    // var frequency = "";
+
+
+    $("#submit").on('click', function() {
+        event.preventDefault();
+
+        var name = $("#name").val().trim();
+        var destination = $("#destination").val().trim();
+        var firstTrain = $("#firstTrain").val().trim();
+        var frequency = $("#frequency").val().trim();
+
+        database.ref().push({
+            name: name,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency
+        });
+
+        $("#name").val("");
+        $("#destination").val("");
+        $("#firstTrain").val("");
+        $("#frequency").val("");
+
+    });
+
+    database.ref().on('child_added', function(snapshot) {
+
+        $("#table").append(
+            "<tr><td>" + snapshot.val().name +
+            "</td><td>" + snapshot.val().destination +
+            "</td><td>" + snapshot.val().frequency +
+            // "</td><td>" + snapshot.val(). +
+            // "</td><td>" + snapshot.val().name + 
+            // "</td><td>" + snapshot.val().name + 
+            "</td></tr>");
+    });
+
+});
